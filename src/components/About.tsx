5fc92c9a9c21bdc9532b555b0bac9b62b0cab34a
midogo21-netizen <midogo21@gmail.com>
2026-01-20 11:25:49 +0100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import workshopImage from "@/assets/gallery/group-mind.jpg";
 
 const About = () => {
   return (
-    <section id="about" className="section-padding pt-4 pb-8">
+    <section id="about" className="section-padding pt-4 pb-10 -mt-6">
       <div className="container-narrow mx-auto">
         {/* Intro Section */}
         <div className="max-w-3xl mx-auto text-center space-y-6 mb-16">
